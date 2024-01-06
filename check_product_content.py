cd3b531864bd55b95bbb39f069d59c65b0b04c86
Ali Emre Nebiler <emre_nebiler@windowslive.com>
2024-01-06 15:03:48 +0300

    updated config handling, increased readability

--- check_product_content.py
+++ check_product_content.py
@@ -14,117 +14,149 @@ from models.account_credentials import AccountCredentials
 from models.excel_cells import ExcelCells
 from models.excel_file import ExcelFile
 from services.product_content_service import ProductContentService
-from utils.excel_utils import (
-    get_column_data_from_excel_sheet,
-    open_workbook,
-)
 
 # ----------------------------
 # COFIGURATIONS
 # ----------------------------
-EXCEL_FILE_PATH = "/path/to/file.xlsx"
-SHEET_NAME = "sheet_name"
-PRODUCTS_ROW_NUMBER_START = 0
-PRODUCTS_ROW_NUMBER_END = 0
-PRODUCT_CODES_COL_NUMBER = 0
-PRODUCT_NAMES_COL_NUMBER = 0
-WEBSITES = {
-    "trendyol": {
-        "username": "user",
-        "password": "pswrd",
-        "url_col_number": 0,
-        "name_col_number": 0,
-        "desc_col_number": 0,
-        "image_col_number": 0,
+CONFIG = {
+    "credentials": {
+        "omniens": {
+            "username": "",
+            "password": "",
+        },
+        "trendyol": {
+            "username": "",
+            "password": "",
+        },
+        "hepsiburada": {
+            "username": "",
+            "password": "",
+        },
     },
-    "hepsiburada": {
-        "username": "user",
-        "password": "pswrd",
-        "url_col_number": 0,
-        "name_col_number": 0,
-        "desc_col_number": 0,
-        "image_col_number": 0,
+    "excel": {
+        "file_path": "",
+        "sheet_name": "",
+        "products": {
+            "code_column": 0,
+            "name_column": 0,
+            "row_start": 0,
+            "row_end": 0,
+        },
+        "trendyol": {
+            "url_column": 0,
+            "name_column": 0,
+            "desc_column": 0,
+            "image_column": 0,
+        },
+        "hepsiburada": {
+            "url_column": 0,
+            "name_column": 0,
+            "desc_column": 0,
+            "image_column": 0,
+        },
     },
 }
 
-excel_file = ExcelFile(
-    file_path=EXCEL_FILE_PATH,
-    sheet_name=SHEET_NAME,
-)
-
-product_code_cells = ExcelCells(
-    column_start=PRODUCT_CODES_COL_NUMBER,
-    row_start=PRODUCTS_ROW_NUMBER_START,
-    row_end=PRODUCTS_ROW_NUMBER_END,
-)
-
+# ----------------------------
+# CREDENTIALS
+# ----------------------------
 trendyol_credentials = AccountCredentials(
-    username=WEBSITES["trendyol"]["username"],
-    password=WEBSITES["trendyol"]["password"],
+    username=CONFIG["credentials"]["trendyol"]["username"],
+    password=CONFIG["credentials"]["trendyol"]["password"],
 )
-
-trendyol_product_url_cells = ExcelCells(
-    column_start=WEBSITES["trendyol"]["url_col_number"],
-    row_start=PRODUCTS_ROW_NUMBER_START,
-)
-
 hepsiburada_credentials = AccountCredentials(
-    username=WEBSITES["hepsiburada"]["username"],
-    password=WEBSITES["hepsiburada"]["password"],
+    username=CONFIG["credentials"]["hepsiburada"]["username"],
+    password=CONFIG["credentials"]["hepsiburada"]["password"],
+)
+omniens_credentials = AccountCredentials(
+    CONFIG["credentials"]["omniens"]["username"],
+    CONFIG["credentials"]["omniens"]["password"],
 )
 
+# ----------------------------
+# EXCEL FILE
+# ----------------------------
+excel_file = ExcelFile(
+    file_path=CONFIG["excel"]["file_path"],
+    sheet_name=CONFIG["excel"]["sheet_name"],
+)
+
+# ----------------------------
+# EXCEL CELLS
+# ----------------------------
+product_code_cells = ExcelCells(
+    column_start=CONFIG["excel"]["products"]["code_column"],
+    row_start=CONFIG["excel"]["products"]["row_start"],
+    row_end=CONFIG["excel"]["products"]["row_end"],
+)
+trendyol_product_name_cells = ExcelCells(
+    column_start=CONFIG["excel"]["trendyol"]["name_column"],
+    row_start=CONFIG["excel"]["products"]["row_start"],
+    row_end=CONFIG["excel"]["products"]["row_end"],
+)
+trendyol_product_desc_cells = ExcelCells(
+    column_start=CONFIG["excel"]["trendyol"]["desc_column"],
+    row_start=CONFIG["excel"]["products"]["row_start"],
+    row_end=CONFIG["excel"]["products"]["row_end"],
+)
+trendyol_product_url_cells = ExcelCells(
+    column_start=CONFIG["excel"]["trendyol"]["url_column"],
+    row_start=CONFIG["excel"]["products"]["row_start"],
+    row_end=CONFIG["excel"]["products"]["row_end"],
+)
+hepsiburada_product_name_cells = ExcelCells(
+    column_start=CONFIG["excel"]["hepsiburada"]["name_column"],
+    row_start=CONFIG["excel"]["products"]["row_start"],
+    row_end=CONFIG["excel"]["products"]["row_end"],
+)
+hepsiburada_product_desc_cells = ExcelCells(
+    column_start=CONFIG["excel"]["hepsiburada"]["desc_column"],
+    row_start=CONFIG["excel"]["products"]["row_start"],
+    row_end=CONFIG["excel"]["products"]["row_end"],
+)
 hepsiburada_product_url_cells = ExcelCells(
-    column_start=WEBSITES["hepsiburada"]["url_col_number"],
-    row_start=PRODUCTS_ROW_NUMBER_START,
+    column_start=CONFIG["excel"]["hepsiburada"]["url_column"],
+    row_start=CONFIG["excel"]["products"]["row_start"],
+    row_end=CONFIG["excel"]["products"]["row_end"],
 )
 
 
 def main():
     "Main code"
 
-    product_content_service = ProductContentService()
+    pc_service = ProductContentService()
 
-    workbook = open_workbook(EXCEL_FILE_PATH)
-    sheet = workbook[SHEET_NAME]
-
-    product_codes = get_column_data_from_excel_sheet(
-        sheet,
-        product_code_cells.column_start,
-        product_code_cells.row_start,
-        product_code_cells.row_end,
+    product_codes = pc_service.get_product_codes_from_excel(
+        excel_file,
+        product_code_cells,
     )
 
     browser = Chrome()
     browser.maximize_window()
 
     try:
-        trendyol_product_urls = (
-            product_content_service.save_trendyol_product_urls_to_excel(
-                browser,
-                trendyol_credentials,
-                excel_file,
-                trendyol_product_url_cells,
-                product_codes,
-            )
+        trendyol_product_urls = pc_service.save_trendyol_product_urls_to_excel(
+            browser,
+            trendyol_credentials,
+            excel_file,
+            trendyol_product_url_cells,
+            product_codes,
         )
 
-        hepsiburada_product_urls = (
-            product_content_service.save_hepsiburada_product_urls_to_excel(
-                browser,
-                hepsiburada_credentials,
-                excel_file,
-                hepsiburada_product_url_cells,
-                product_codes,
-            )
+        hepsiburada_product_urls = pc_service.save_hepsiburada_product_urls_to_excel(
+            browser,
+            hepsiburada_credentials,
+            excel_file,
+            hepsiburada_product_url_cells,
+            product_codes,
         )
 
     except Exception:
         print("An unexpected error occured.")
 
-    workbook.close()
-    browser.close()
-
     print("Completed.")
+
+    browser.close()
 
 
 if __name__ == "__main__":
