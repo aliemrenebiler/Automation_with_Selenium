b80f9235a9f728ddc6cf305e955a7dd510cfe072
Ali Emre Nebiler <emre_nebiler@windowslive.com>
2024-01-07 00:30:55 +0300

    added custom error models

---
+++ models/errors.py
@@ -0,0 +1,34 @@
+"Error Models"
+
+
+class GenericError(Exception):
+    "Generic Error"
+
+    def __init__(self, message: str = ""):
+        class_name = self.__class__.__name__
+        self.message = f"[{class_name}] {message}" if message else f"[{class_name}]"
+        super().__init__(self.message)
+
+
+class LoginError(GenericError):
+    "Login Error"
+
+    def __init__(self, message: str = ""):
+        self.message = message
+        super().__init__(self.message)
+
+
+class WebDriverError(GenericError):
+    "Web Driver Error"
+
+    def __init__(self, message: str = ""):
+        self.message = message
+        super().__init__(self.message)
+
+
+class JinjaError(GenericError):
+    "Jinja Error"
+
+    def __init__(self, message: str = ""):
+        self.message = message
+        super().__init__(self.message)
