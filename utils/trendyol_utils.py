6cd1194ba8d2aaaa3a718bf7229a906e31b08ba0
Ali Emre Nebiler <emre_nebiler@windowslive.com>
2024-01-07 00:31:45 +0300

    updated trendyol utils for error handling

--- utils/trendyol_utils.py
+++ utils/trendyol_utils.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+from models.errors import LoginError, WebDriverError
 from models.web_driver import WebDriver
 
 # pylint: disable=broad-except
@@ -14,43 +15,46 @@ def login_to_trendyol(
 ):
     "Logins to Trendyol"
 
-    browser.get("https://partner.trendyol.com/account/login")
+    try:
+        browser.get("https://partner.trendyol.com/account/login")
 
-    WebDriverWait(browser, timeout).until(
-        EC.presence_of_element_located(
-            (
-                By.XPATH,
-                '//div[@class="email-phone g-input"]//input',
+        WebDriverWait(browser, timeout).until(
+            EC.presence_of_element_located(
+                (
+                    By.XPATH,
+                    '//div[@class="email-phone g-input"]//input',
+                )
+            )
+        ).send_keys(email)
+
+        WebDriverWait(browser, timeout).until(
+            EC.presence_of_element_located(
+                (
+                    By.XPATH,
+                    '//div[@class="password g-input"]//input',
+                )
+            )
+        ).send_keys(password)
+
+        WebDriverWait(browser, timeout).until(
+            EC.presence_of_element_located(
+                (
+                    By.XPATH,
+                    '//button[@class="invisible-captcha-btn btn '
+                    + 'btn-lg btn-mp-primary btn-block g-button -primary"]',
+                )
+            )
+        ).click()
+
+        WebDriverWait(browser, timeout).until(
+            EC.url_to_be("https://partner.trendyol.com/dashboard")
+            or EC.url_to_be(
+                "https://partner.trendyol.com/account/info"
+                + "?tab=contractAndDocuments&openApproveModal=true"
             )
         )
-    ).send_keys(email)
-
-    WebDriverWait(browser, timeout).until(
-        EC.presence_of_element_located(
-            (
-                By.XPATH,
-                '//div[@class="password g-input"]//input',
-            )
-        )
-    ).send_keys(password)
-
-    WebDriverWait(browser, timeout).until(
-        EC.presence_of_element_located(
-            (
-                By.XPATH,
-                '//button[@class="invisible-captcha-btn btn '
-                + 'btn-lg btn-mp-primary btn-block g-button -primary"]',
-            )
-        )
-    ).click()
-
-    WebDriverWait(browser, timeout).until(
-        EC.url_to_be("https://partner.trendyol.com/dashboard")
-        or EC.url_to_be(
-            "https://partner.trendyol.com/account/info"
-            + "?tab=contractAndDocuments&openApproveModal=true"
-        )
-    )
+    except Exception as exc:
+        raise LoginError("Could not login to Trendyol.") from exc
 
 
 def get_product_url_from_trendyol(
@@ -63,7 +67,10 @@ def get_product_url_from_trendyol(
             != "https://partner.trendyol.com/product-listing/all-products"
         ):
             browser.get("https://partner.trendyol.com/product-listing/all-products")
+    except Exception as exc:
+        raise WebDriverError("Could not found Trendyol partner products page.") from exc
 
+    try:
         WebDriverWait(browser, timeout).until(
             EC.presence_of_element_located(
                 (
@@ -81,7 +88,10 @@ def get_product_url_from_trendyol(
                 )
             )
         ).click()
+    except Exception as exc:
+        raise WebDriverError("Error during Trendyol product search.") from exc
 
+    try:
         product_url = (
             WebDriverWait(browser, timeout)
             .until(
@@ -102,23 +112,31 @@ def get_product_url_from_trendyol(
 
 def get_product_info_from_trendyol(
     browser: WebDriver, product_url: str, timeout: int = 3
-) -> (str, str | None):
+) -> (str | None, str | None):
     "Gets the product name and description on Trendyol"
 
-    browser.get(product_url)
+    try:
+        browser.get(product_url)
+    except Exception as exc:
+        raise WebDriverError(
+            "Could not get product information, could not reach Trendyol product page."
+        ) from exc
 
-    product_name = (
-        WebDriverWait(browser, timeout)
-        .until(
-            EC.presence_of_element_located(
-                (
-                    By.XPATH,
-                    '//div[@class="product-detail-wrapper"]//h1[@class="pr-new-br"]/span',
+    try:
+        product_name = (
+            WebDriverWait(browser, timeout)
+            .until(
+                EC.presence_of_element_located(
+                    (
+                        By.XPATH,
+                        '//div[@class="product-detail-wrapper"]//h1[@class="pr-new-br"]/span',
+                    )
                 )
             )
+            .text
         )
-        .text
-    )
+    except Exception:
+        product_name = None
 
     try:
         product_desc_child_elements = (
