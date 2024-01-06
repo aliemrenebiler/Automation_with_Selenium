ff0bc1edd370ee09a92dcf1d78fc568fbf9224d5
Ali Emre Nebiler <emre_nebiler@windowslive.com>
2024-01-07 00:55:37 +0300

    updated hepsiburada utils for error handling, changed cookie handling funciton name to accept_hepsiburada_cookies

--- utils/hepsiburada_utils.py
+++ utils/hepsiburada_utils.py
@@ -3,6 +3,7 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from models.errors import LoginError, WebDriverError
 
 from models.web_driver import WebDriver
 
@@ -14,91 +15,82 @@ def login_to_hepsiburada(
 ):
     "Logins to Hepsiburada"
 
-    browser.get("https://merchant.hepsiburada.com/v2/login")
-
-    WebDriverWait(browser, timeout).until(
-        EC.presence_of_element_located(
-            (
-                By.XPATH,
-                '//input[@id="username"]',
-            )
-        )
-    ).send_keys(email)
-
-    WebDriverWait(browser, timeout).until(
-        EC.presence_of_element_located(
-            (
-                By.XPATH,
-                '//button[@id="merchant-sign-in-button"]',
-            )
-        )
-    ).click()
-
-    WebDriverWait(browser, timeout).until(
-        EC.presence_of_element_located(
-            (
-                By.XPATH,
-                '//input[@id="password"]',
-            )
-        )
-    ).send_keys(password)
-
-    WebDriverWait(browser, timeout).until(
-        EC.presence_of_element_located(
-            (
-                By.XPATH,
-                '//button[@id="merchant-sign-in-button"]',
-            )
-        )
-    ).click()
-
-    WebDriverWait(browser, timeout).until(
-        EC.url_to_be("https://merchant.hepsiburada.com/v2/dashboard")
-        or EC.url_to_be("https://merchant.hepsiburada.com/v2/listing?tab=onSale")
-    )
-
-
-def handle_hepsiburada_cookie(browser: WebDriver, timeout: int = 3):
-    "Handles Hepsiburada cookie popup"
-
     try:
-        browser.get("https://www.hepsiburada.com/")
+        browser.get("https://merchant.hepsiburada.com/v2/login")
 
         WebDriverWait(browser, timeout).until(
             EC.presence_of_element_located(
                 (
                     By.XPATH,
-                    '//button[@id="onetrust-accept-btn-handler"]',
+                    '//input[@id="username"]',
+                )
+            )
+        ).send_keys(email)
+
+        WebDriverWait(browser, timeout).until(
+            EC.presence_of_element_located(
+                (
+                    By.XPATH,
+                    '//button[@id="merchant-sign-in-button"]',
                 )
             )
         ).click()
-    except Exception:
-        ...
+
+        WebDriverWait(browser, timeout).until(
+            EC.presence_of_element_located(
+                (
+                    By.XPATH,
+                    '//input[@id="password"]',
+                )
+            )
+        ).send_keys(password)
+
+        WebDriverWait(browser, timeout).until(
+            EC.presence_of_element_located(
+                (
+                    By.XPATH,
+                    '//button[@id="merchant-sign-in-button"]',
+                )
+            )
+        ).click()
+
+        WebDriverWait(browser, timeout).until(
+            EC.url_to_be("https://merchant.hepsiburada.com/v2/dashboard")
+            or EC.url_to_be("https://merchant.hepsiburada.com/v2/listing?tab=onSale")
+        )
+    except Exception as exc:
+        raise LoginError("Could not login to Hepsiburada.") from exc
 
 
 def get_product_url_from_hepsiburada(
     browser: WebDriver, product_code: str, timeout: int = 8
 ) -> str | None:
     "Gets the URL of the product with specified code from Hepsiburada"
+
     try:
         browser.get(
             "https://merchant.hepsiburada.com/v2/listings?"
             + f"tab=all&page=1&pageSize=10&search={product_code}"
         )
+    except Exception as exc:
+        raise WebDriverError(
+            "Could not found Hepsiburada merchant products page."
+        ) from exc
 
-        try:
-            WebDriverWait(browser, timeout).until(
-                EC.presence_of_element_located(
-                    (
-                        By.XPATH,
-                        '//div[@class="no-data-placeholder"]',
-                    )
+    try:
+        WebDriverWait(browser, timeout).until(
+            EC.presence_of_element_located(
+                (
+                    By.XPATH,
+                    '//div[@class="no-data-placeholder"]',
                 )
             )
-            return None
-        except Exception:
-            ...
+        )
+        return None
+    except Exception:
+        ...
 
+    try:
         product_url = (
             WebDriverWait(browser, timeout)
             .until(
@@ -111,31 +103,39 @@ def get_product_url_from_hepsiburada(
             )
             .get_attribute("href")
         )
-
-        return product_url
     except Exception:
-        return None
+        product_url = None
+
+    return product_url
 
 
 def get_product_info_from_hepsiburada(
     browser: WebDriver, product_url: str, timeout: int = 10
-) -> (str, str | None):
+) -> (str | None, str | None):
     "Gets the product name and description on Hepsiburada"
 
-    browser.get(product_url)
+    try:
+        browser.get(product_url)
+    except Exception as exc:
+        raise WebDriverError(
+            "Could not get product information, could not reach Hepsiburada product page."
+        ) from exc
 
-    product_name = (
-        WebDriverWait(browser, timeout)
-        .until(
-            EC.presence_of_element_located(
-                (
-                    By.XPATH,
-                    '//h1[@id="product-name"]',
+    try:
+        product_name = (
+            WebDriverWait(browser, timeout)
+            .until(
+                EC.presence_of_element_located(
+                    (
+                        By.XPATH,
+                        '//h1[@id="product-name"]',
+                    )
                 )
             )
+            .text
         )
-        .text
-    )
+    except Exception:
+        product_name = None
 
     try:
         product_desc_child_elements = (
@@ -158,3 +158,21 @@ def get_product_info_from_hepsiburada(
         product_desc = None
 
     return product_name, product_desc
+
+
+def accept_hepsiburada_cookies(browser: WebDriver, timeout: int = 10):
+    "Accepts Hepsiburada cookies"
+
+    try:
+        browser.get("https://www.hepsiburada.com/")
+
+        WebDriverWait(browser, timeout).until(
+            EC.presence_of_element_located(
+                (
+                    By.XPATH,
+                    '//button[@id="onetrust-accept-btn-handler"]',
+                )
+            )
+        ).click()
+    except Exception as exc:
+        raise WebDriverError("Error during accepting Hepsiburada cookies.") from exc
