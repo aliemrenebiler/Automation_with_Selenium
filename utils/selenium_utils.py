ed5576e3e796b61a7adda4e047712b73235e7e90
Ali Emre Nebiler <emre_nebiler@windowslive.com>
2024-01-06 19:03:43 +0300

    added chrome disable notifications

--- utils/selenium_utils.py
+++ utils/selenium_utils.py
@@ -6,8 +6,13 @@ from selenium.webdriver import Chrome, ChromeOptions
 def create_chrome_browser() -> Chrome:
     "Creates a Chrome web browser"
 
+    prefs = {"profile.default_content_setting_values.notifications": 2}
+
     options = ChromeOptions()
     options.add_argument("--ignore-certificate-errors")
     options.add_argument("--ignore-ssl-errors")
+    options.add_experimental_option("prefs", prefs)
+
     browser = Chrome(options=options)
+
     return browser
