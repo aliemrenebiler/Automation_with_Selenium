d93f572ad21fdd58e49227204cf0c3b42c813b2c
Ali Emre Nebiler <emre_nebiler@windowslive.com>
2024-01-07 00:31:23 +0300

    updated jinja utils for error handling

--- utils/jinja_utils.py
+++ utils/jinja_utils.py
@@ -2,6 +2,8 @@
 
 from jinja2 import Environment, FileSystemLoader
 
+from models.errors import JinjaError
+
 
 def create_html_from_jinja_template(
     template_folder_path: str,
@@ -10,7 +12,10 @@ def create_html_from_jinja_template(
 ) -> str:
     "Creates HTML from Jinja template"
 
-    environment = Environment(loader=FileSystemLoader(template_folder_path))
-    template = environment.get_template(template_file_name)
-    html = template.render(**template_variables)
+    try:
+        environment = Environment(loader=FileSystemLoader(template_folder_path))
+        template = environment.get_template(template_file_name)
+        html = template.render(**template_variables)
+    except Exception as exc:
+        raise JinjaError(str(exc)) from exc
     return html
