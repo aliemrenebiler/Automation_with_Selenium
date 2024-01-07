a075404eb5751346e1ff364ee86899674bda1a8f
Ali Emre Nebiler <emre_nebiler@windowslive.com>
2024-01-08 02:46:13 +0300

    updated config constants

--- common/constants/file_and_folder_paths.py
+++ common/constants/file_and_folder_paths.py
@@ -4,17 +4,17 @@ import os
 
 # Configuration Paths
 COMPARE_OMNIENS_PRODUCT_INFOS_WITH_TY_AND_HB_CONFIG_FILE_PATH = os.path.join(
-    "config",
+    "configs",
     "workflow_configs",
     "compare_omniens_product_infos_with_ty_and_hb.json",
 )
 SAVE_TY_PARTNER_PRODUCT_URLS_TO_EXCEL_CONFIG_FILE_PATH = os.path.join(
-    "config",
+    "configs",
     "workflow_configs",
     "save_trendyol_partner_product_urls_to_excel.json",
 )
 SAVE_HB_MERCHANT_PRODUCT_URLS_TO_EXCEL_CONFIG_FILE_PATH = os.path.join(
-    "config",
+    "configs",
     "workflow_configs",
     "save_hepsiburada_merchant_product_urls_to_excel.json",
 )
