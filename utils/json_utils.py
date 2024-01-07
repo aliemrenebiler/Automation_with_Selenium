0704fb0adfb311ad43fbbf89ca3724cee04f3d23
Ali Emre Nebiler <emre_nebiler@windowslive.com>
2024-01-07 18:30:44 +0300

    added json utils for configurations

---
+++ utils/json_utils.py
@@ -0,0 +1,17 @@
+"JSON Utils"
+
+import json
+from jsonschema import validate
+
+
+def get_json_file_content(file_path: str) -> dict:
+    "Gets JSON file content and return it as dictionary"
+
+    with open(file_path, "r", encoding="utf8") as file:
+        return json.load(file)
+
+
+def validate_with_json_schema(content: dict, schema: dict):
+    "Validates dictionary with JSON schema"
+
+    validate(content, schema)
