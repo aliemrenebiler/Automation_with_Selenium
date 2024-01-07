1fccb5f133465d5beeee3c72043094240a69a4bb
Ali Emre Nebiler <emre_nebiler@windowslive.com>
2024-01-08 01:23:32 +0300

    updated excel models

--- models/excel.py
+++ models/excel.py
@@ -1,9 +1,33 @@
-"Excel File Model"
+"Excel Models"
 
 from openpyxl import Workbook
 from openpyxl.worksheet.worksheet import Worksheet
 
 # pylint: disable=too-few-public-methods
+
+
+class ExcelRange:
+    "Excel Range Model Class"
+
+    def __init__(
+        self,
+        start: int,
+        end: int,
+    ):
+        self.start = start
+        self.end = end
+
+
+class ExcelCells:
+    "Excel Cells Model Class"
+
+    def __init__(
+        self,
+        row: int | ExcelRange,
+        column: int | ExcelRange,
+    ):
+        self.row = row
+        self.column = column
 
 
 class ExcelFile:
