28a1bc0503d51eab9aca1f1091c06df68d440a7d
Ali Emre Nebiler <emre_nebiler@windowslive.com>
2024-01-08 00:38:17 +0300

    added url constants

---
+++ common/constants/urls.py
@@ -0,0 +1,31 @@
+"URLs"
+
+# pylint: disable=line-too-long
+
+
+# Omniens URLs
+OMNIENS_LOGIN_PAGE_URL = "https://platform.omniens.com/login"
+OMNIENS_DASHBOARD_PAGE_URL = "https://platform.omniens.com/performance-dashboard"
+OMNIENS_PRODUCTS_PAGE_URL = "https://platform.omniens.com/_product/product/list"
+
+# Trendyol URLs
+TRENDYOL_MAIN_PAGE_URL = "https://www.trendyol.com/"
+TRENDYOL_PARTNER_LOGIN_PAGE_URL = "https://partner.trendyol.com/account/login"
+TRENDYOL_PARTNER_DASHBOARD_PAGE_URL = "https://partner.trendyol.com/dashboard"
+TRENDYOL_PARTNER_REDIRECT_AFTER_LOGIN_PAGE_URL = "https://partner.trendyol.com/account/info?tab=contractAndDocuments&openApproveModal=true"
+TRENDYOL_PARTNER_PRODUCTS_PAGE_URL = (
+    "https://partner.trendyol.com/product-listing/all-products"
+)
+
+# Hepsiburada URLs
+HEPSIBURADA_MAIN_PAGE_URL = "https://www.hepsiburada.com/"
+HEPSIBURADA_MERCHANT_LOGIN_PAGE_URL = "https://merchant.hepsiburada.com/v2/login"
+HEPSIBURADA_MERCHANT_DASHBOARD_PAGE_URL = (
+    "https://merchant.hepsiburada.com/v2/dashboard"
+)
+HEPSIBURADA_MERCHANT_REDIRECT_AFTER_LOGIN_PAGE_URL = (
+    "https://merchant.hepsiburada.com/v2/listing?tab=onSale"
+)
+HEPSIBURADA_MERCHANT_PRODUCT_SEARCH_URL = (
+    "https://merchant.hepsiburada.com/v2/listings?tab=all&page=1&pageSize=10&search="
+)
