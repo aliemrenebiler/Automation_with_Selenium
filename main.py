fd5273c167583d7436d83b3f2fa46dd0a56d2298
Ali Emre Nebiler <emre_nebiler@windowslive.com>
2024-01-08 02:55:41 +0300

    added all workflows to main function, added a small text based UI

--- main.py
+++ main.py
@@ -1,13 +1,39 @@
 "Main"
 
-from workflows.product_content_workflows import ProductContentWorkflows
+
+from workflows.compare_omniens_product_infos_with_ty_and_hb import (
+    compare_omniens_product_infos_with_ty_and_hb,
+)
+from workflows.save_hepsiburada_merchant_product_urls_to_excel import (
+    save_hepsiburada_merchant_product_urls_to_excel,
+)
+from workflows.save_trendyol_partner_product_urls_to_excel import (
+    save_trendyol_partner_product_urls_to_excel,
+)
 
 
 def main():
     "Main Function"
 
-    pc_workflows = ProductContentWorkflows()
-    pc_workflows.compare_product_information()
+    print()
+    print("=== E-Marketing Management System (EMMA) ===\n")
+
+    print("Product Content:")
+    print("[1] Compare Omniens Product Informations With Trendyol And Hepsiburada")
+    print("[2] Save Trendyol Partner Product URLs To Excel")
+    print("[3] Save Hepsiburada Merchant Product URLs To Excel\n")
+
+    selection = int(input("Selection: "))
+    print()
+
+    if selection == 1:
+        compare_omniens_product_infos_with_ty_and_hb()
+    elif selection == 2:
+        save_trendyol_partner_product_urls_to_excel()
+    elif selection == 3:
+        save_hepsiburada_merchant_product_urls_to_excel()
+    else:
+        print("(!) Invalid selection.")
 
 
 if __name__ == "__main__":
