fb50734c5538bbde58ecbe9c739eb85df97341a1
Ali Emre Nebiler <emre_nebiler@windowslive.com>
2024-01-07 22:40:55 +0300

    added config error model

--- models/errors.py
+++ models/errors.py
@@ -10,6 +10,14 @@ class GenericError(Exception):
         super().__init__(self.message)
 
 
+class ConfigError(GenericError):
+    "Config Error"
+
+    def __init__(self, message: str = ""):
+        self.message = message
+        super().__init__(self.message)
+
+
 class LoginError(GenericError):
     "Login Error"
 
